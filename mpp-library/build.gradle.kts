6dba4b504f696c37d413607e6584b6d1e48c86c0
Andrew Kovalev <kovalev@icerockdev.com>
2022-01-27 12:15:45 +0700

    update plugin verion. Add task for sourceSets print

--- mpp-library/build.gradle.kts
+++ mpp-library/build.gradle.kts
@@ -22,14 +22,26 @@
      ios()
      android()
 
-     sourceSets.configureEach { println("Configure: $name") }
-
      sourceSets {
          val commonMain by getting
          commonMain.dependencies {
              implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.10")
              api(project(":mpp-library:feature-auth"))
              api(project(":mpp-library:feature-profile"))
+         }
+     }
+
+     allprojects {
+         tasks.create("PrintSourceSets") {
+             group = "source-sets-printer"
+
+             doFirst {
+                 println("=== SOURCE SETS PRINTER 2000 ===")
+                 kotlin {
+                     sourceSets.forEach { println("SourceSet name: ${it.name}") }
+                     println("=== SOURCE SETS END. TOTAL: ${kotlin.sourceSets.size} ITEMS ===")
+                 }
+             }
          }
      }
  }
--- mpp-library/feature-auth/build.gradle.kts
+++ mpp-library/feature-auth/build.gradle.kts
@@ -24,7 +24,7 @@
      sourceSets {
          val commonMain by getting
          commonMain.dependencies {
-             implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.5.30")
+             implementation("org.jetbrains.kotlin:kotlin-stdlib-common:1.6.10")
          }
      }
  }
